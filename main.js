2a1754eceb24212d58c576bcfe7469156d059444
Kiran Chhablani <chhablanikiran@gmail.com>
2021-12-22 18:59:40 +0530

    commit - 1

---
+++ main.js
@@ -0,0 +1,17 @@
+const navToggle=document.querySelector(".nav-toggle");
+const links=document.querySelector(".nav-links");
+
+navToggle.addEventListener("click", function(){
+// console.log(links.classList);
+// if(links.classList.contains("show-links")){
+//  links.classList.remove("show-links");
+// }
+// else{
+//  links.classList.add("show-links");
+// }
+ links.classList.toggle("show-links");
+
+
+
+
+});
